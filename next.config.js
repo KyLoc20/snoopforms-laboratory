20b9bfd5e9b162cbef370c1f381b220a6572e1a9
Kyloc21 <liuzeduo1234@gmail.com>
2022-08-13 15:40:29 +0800

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+var path = require("path");
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: false,
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        nextauthSecret: process.env.NEXTAUTH_SECRET,
+        nextauthUrl: process.env.NEXTAUTH_URL,
+        mailFrom: process.env.MAIL_FROM,
+        smtpHost: process.env.SMTP_HOST,
+        smtpPort: process.env.SMTP_PORT,
+        smtpUser: process.env.SMTP_USER,
+        smtpPassword: process.env.SMTP_PASSWORD,
+        smtpSecureEnabled: process.env.SMTP_SECURE_ENABLED,
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        posthogApiHost: process.env.POSTHOG_API_HOST,
+        posthogApiKey: process.env.POSTHOG_API_KEY,
+        termsUrl: process.env.TERMS_URL,
+        privacyUrl: process.env.PRIVACY_URL,
+    },
+    // async redirects() {
+    //     return [{
+    //         source: "/",
+    //         destination: "/forms/",
+    //         permanent: false,
+    //     }, ];
+    // },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.resolve.alias["react"] = path.resolve("./node_modules/react");
+        // Important: return the modified config
+        return config;
+    },
+};
+
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import App, { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import Head from "next/head";
+import "highlight.js/styles/tokyo-night-dark.css";
+import "../styles/globals.css";
+import "../styles/editorjs.css";
+import "../styles/toastify.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function SnoopApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Snoopforms Lab</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
+// import { SessionProvider } from "next-auth/react";
+// import { usePosthog } from "../lib/posthog";
+// function SnoopAppWithAuth({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+//   // usePosthog();
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//       <ToastContainer />
+//     </SessionProvider>
+//   );
+// }
+SnoopApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
 
-export default MyApp
+  return { ...appProps };
+};
+
+export default SnoopApp;
