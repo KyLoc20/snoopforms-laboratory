6d1d7f3f8c1b2325430bb2990d491c22b33f244e
Kyloc21 <liuzeduo1234@gmail.com>
2022-09-05 15:28:42 +0800

    ref: NotFound

--- components/layout/NotFound/index.tsx
+++ components/layout/NotFound/index.tsx
@@ -1,3 +1,9 @@
 export function FormNotFound({ formId }: { formId: string }) {
-  return <div style={{}}>{formId} Not Found</div>;
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+      <div style={{ padding: "24px 0" }}>
+        Ooops, that Form of <i>{formId}</i> is NOT here.
+      </div>
+    </div>
+  );
 }
