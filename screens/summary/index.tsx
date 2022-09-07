07e43c06e565a310092093bdf09bf9a6a904a25e
Kyloc21 <liuzeduo1234@gmail.com>
2022-09-08 01:52:48 +0800

    ref: SummaryScreen layout

--- screens/summary/index.tsx
+++ screens/summary/index.tsx
@@ -1,31 +1,36 @@
-import { NavBar } from "@/components/layout/Navigation";
-import { Container, MaxWidth, Loading, FormNotFound } from "@/components/layout";
+import { TopBarNavagationFullAppLayout, Loading, FormNotFound } from "@/components/layout";
 import { useSubmissionSessions } from "@/lib/submissionSession";
 import { useFormIdSafely } from "@/lib/router";
 import SummaryApp from "@/components/frontend/SummaryApp";
 import { useNoCodeForm } from "@/lib/noCodeForm";
-import TopBar from "@/components/TopBar";
 export default function Screen() {
   const { formId, isValid } = useFormIdSafely();
-  const { isLoading, hasError } = useSubmissionSessions(formId);
-  const { isLoading: isLoadingForm, hasError: hasErrorForm, noCodeForm } = useNoCodeForm(formId);
-  const isReady = isValid && !isLoading;
-  const shouldShowName = isReady && !isLoadingForm && !hasErrorForm;
-  console.log("SummaryApp", shouldShowName, noCodeForm.name);
-  return (
-    <Container bg="rgb(246, 248, 249, 1)">
-      <TopBar title={shouldShowName ? noCodeForm.name : ""} />
-      <NavBar currentNav="summary" formId={formId}></NavBar>
-      <MaxWidth>
-        <R formId={formId as string} isReady={isReady} hasError={hasError}></R>
-      </MaxWidth>
-      <div id="new-form-modal"></div>
-    </Container>
-  );
+  if (isValid) return <View formId={formId as string} />;
+  else return <LoadingView />;
 }
 function R({ formId, isReady, hasError }: { formId: string; isReady: boolean; hasError: boolean }) {
   if (isReady) {
     if (hasError) return <FormNotFound formId={formId} />;
     else return <SummaryApp formId={formId} />;
   } else return <Loading />;
+}
+
+function View({ formId }: { formId: string }) {
+  const { isLoading, hasError } = useSubmissionSessions(formId);
+  const { isLoading: isLoadingForm, hasError: hasErrorForm, noCodeForm } = useNoCodeForm(formId);
+  const isReady = !isLoading;
+  const hasName = isReady && !isLoadingForm && !hasErrorForm;
+  return (
+    <TopBarNavagationFullAppLayout title={hasName ? noCodeForm.name : ""} currentNav={"summary"} formId={formId}>
+      <R formId={formId as string} isReady={isReady} hasError={hasError}></R>
+    </TopBarNavagationFullAppLayout>
+  );
+}
+
+function LoadingView() {
+  return (
+    <TopBarNavagationFullAppLayout title={""} currentNav={"summary"}>
+      <Loading />
+    </TopBarNavagationFullAppLayout>
+  );
 }
