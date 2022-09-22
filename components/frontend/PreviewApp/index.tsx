588a46951dc75c5fd97ce1586664ff13fc2a473c
Kyloc21 <liuzeduo1234@gmail.com>
2022-09-23 00:46:24 +0800

    ref: usePages in PreviewApp

--- components/frontend/PreviewApp/index.tsx
+++ components/frontend/PreviewApp/index.tsx
@@ -1,5 +1,4 @@
-import { useMemo, useState } from "react";
-import { generateId } from "@/lib/utils";
+import { useState } from "react";
 import Loading from "@/components/layout/Loading";
 import { BlockData } from "@/lib/types";
 import { toast } from "react-toastify";
@@ -8,32 +7,14 @@ import { SubmissionPage, getQuestionTitleMap } from "../ResponseApp/SubmissionDi
 import clsx from "clsx";
 import { Description, Button } from "@/components/modal/widgets";
 import DownloadButton from "./DownloadButton";
+import usePages from "@/hooks/usePages";
 export default function PreviewApp({ formId, blocks }: { formId: string; blocks: BlockData[] }) {
+  console.log("RENDER PreviewApp", formId, blocks);
   const id2Title = getQuestionTitleMap(blocks);
   const [isCompleted, setIsCompleted] = useState(false);
   const [localSubmissions, setLocalSubmissions] = useState<SubmissionData[]>([]);
   const [whenSubmit, setWhenSubmit] = useState<number | undefined>(undefined);
-
-  console.log("RENDER PreviewApp", formId, blocks);
-  const pages = useMemo(() => {
-    const allPages: Page[] = [];
-    let cPage: Page = { id: generateId(10), blocks: [] };
-    blocks.forEach((cBlock) => {
-      cPage.blocks.push(cBlock);
-      if (cBlock.type === "pageTransition") {
-        //generate a new Page
-        allPages.push(cPage);
-        cPage = { id: generateId(10), blocks: [] };
-      }
-    });
-    //TODO make it configurable
-    //the last Page, add a Done Button by default
-    if (cPage.blocks.length > 0) {
-      cPage.blocks.push({ id: generateId(10), type: "pageTransition", data: { _component: { submitLabel: "DONE" } } });
-      allPages.push(cPage);
-    }
-    return allPages;
-  }, [blocks, formId]);
+  const { pages } = usePages(blocks);
 
   const handleFormCompleted = (submissions: SubmissionData[], when: number) => {
     toast("Congratulations! You Have Finished the PREVIEW Form 🎉", { autoClose: 2000 });
@@ -49,7 +30,6 @@ export default function PreviewApp({ formId, blocks }: { formId: string; blocks:
 
   if (!pages) return <Loading />;
   else {
-    // console.log("RENDER FormApp", pages);
     return (
       <div className={clsx("preview-app", "w-full h-full px-5 py-5 mb-[10vh]")}>
         {isCompleted ? (
