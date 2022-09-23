1103fbfd3d57e0fc30d5a09a7133d667e4d6d81f
Kyloc21 <liuzeduo1234@gmail.com>
2022-09-23 16:20:00 +0800

    ref: SubmissionPagination

--- components/frontend/ResponseApp/SessionDetails/index.tsx
+++ components/frontend/ResponseApp/SessionDetails/index.tsx
@@ -4,31 +4,19 @@ import { createResponseDisplay, isQuestionType } from "@/lib/snoopforms/react/qu
 import { useNoCodeForm } from "@/lib/noCodeForm";
 import Chip from "@/lib/ui/Chip";
 import { shortcut } from "@/lib/snoopforms/react/questions";
-type List<T> = T[];
-type SubmissionPage = List<SubmissionData>;
-type TitleMap = { [questionId: string]: string };
+import { useMemo } from "react";
+import { generateId } from "@/lib/utils";
 
-const paginateSession = (session: SubmissionSessionData): List<SubmissionPage> => {
-  return [session.submissions];
-};
-export const getQuestionTitleMap = (blocks: BlockData[]) => {
-  //init a Map: questionId -> questionTitle
-  const questionTitleMap: TitleMap = {};
-  blocks.forEach((block) => {
-    const { id, type, data } = block;
-    if (isQuestionType(type)) {
-      //todo questionConfig: data._component dangerous
-      questionTitleMap[id] = (data?._component?.title as string) ?? id;
-    }
-  });
-  return questionTitleMap;
-};
 export default function SessionDetails({ formId, submissionSession }: { formId: string; submissionSession: SubmissionSessionData }) {
   //make sure the corresponding Form to be existed
-  // const { form, isLoadingForm } = useForm(formId);
   const { noCodeForm } = useNoCodeForm(formId);
   //pagination
-  const pages = paginateSession(submissionSession);
+  const pages = useMemo(() => {
+    const formPagination = getFormPagination(noCodeForm.blocks);
+    const pages = paginateSession(submissionSession, formPagination);
+    return pages;
+  }, [submissionSession, noCodeForm]);
+  //id title mapping
   const id2Title = getQuestionTitleMap(noCodeForm.blocks);
   return (
     <ul role="list" className={clsx("submission-list", "divide-y divide-ui-gray-light")}>
@@ -40,7 +28,7 @@ export default function SessionDetails({ formId, submissionSession }: { formId: 
 }
 export function SubmissionPage({ submissions, titleMap }: { submissions: SubmissionPage; titleMap: TitleMap }) {
   return (
-    <div style={{ marginBottom: "8px", padding: "8px", border: "dashed 2px rgb(210, 218, 226)", borderRadius: "6px" }}>
+    <div style={{ marginBottom: "16px", padding: "8px", border: "dashed 2px rgb(210, 218, 226)", borderRadius: "6px" }}>
       {submissions.map((submission, i) => (
         <SubmissionDisplay
           key={submission.id}
@@ -62,9 +50,66 @@ function SubmissionDisplay({ submission, questionTitle }: { submission: Submissi
           <Chip label={shortcut(submission.questionType)} hegiht={20} padding="0 6px" fontSize="12px" />
         </span>
       </p>
-      <p className={clsx(submission.details ? "text-gray-600" : "text-gray-400", "pt-1 text-sm text-gray-600 line-clamp-2")}>
+      <p className={clsx(submission.details ? "text-gray-600" : "text-gray-400", "mt-2 text-sm text-gray-600 line-clamp-2")}>
         <RenderDisplay></RenderDisplay>
       </p>
     </li>
   );
 }
+
+type QuestionId = string;
+export const getFormPagination = (blocks: BlockData[]) => {
+  const pages: List<QuestionId[]> = [];
+  let cPage: QuestionId[] = [];
+  blocks.forEach((block) => {
+    const { id, type } = block;
+    if (isQuestionType(type)) {
+      cPage.push(id);
+    }
+    if (type === "pageTransition") {
+      pages.push(cPage);
+      cPage = [];
+    }
+  });
+  if (cPage.length > 0) {
+    pages.push(cPage);
+  }
+  return pages;
+};
+type List<T> = T[];
+type SubmissionPage = List<SubmissionData>;
+const paginateSession = (session: SubmissionSessionData, formPages: List<QuestionId[]>): List<SubmissionPage> => {
+  const submissions = session.submissions;
+  const pages: List<SubmissionPage> = formPages.map((questions) => {
+    const submissionPage = questions.map((questionId) => {
+      const possibleAnswer = submissions.find((answer) => answer.questionId === questionId);
+      if (possibleAnswer !== undefined) {
+        return possibleAnswer;
+      } else {
+        //basically this won't happen cuz all submissions are here
+        return {
+          id: generateId(10),
+          questionId: questionId,
+          questionType: "unknown",
+          details: {},
+        } as SubmissionData;
+      }
+    });
+    return submissionPage;
+  });
+  return pages;
+};
+
+type TitleMap = { [questionId: string]: string };
+export const getQuestionTitleMap = (blocks: BlockData[]) => {
+  //init a Map: questionId -> questionTitle
+  const questionTitleMap: TitleMap = {};
+  blocks.forEach((block) => {
+    const { id, type, data } = block;
+    if (isQuestionType(type)) {
+      //todo questionConfig: data._component dangerous
+      questionTitleMap[id] = (data?._component?.title as string) ?? id;
+    }
+  });
+  return questionTitleMap;
+};
